3f0c9cb1cf43cfeb7692a7c50d098971bfcb0cd3
Jnayakk <jnayakk97@gmail.com>
2017-06-09 00:31:14 -0400

    Nav bar works so that when clicked, it scrolls to corresponding section

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,30 @@
-function main(){
-    alert('Test');
+function main() {
+    alert('test');
+
 }
 
-main();
+function navbarclick() {
+    $('#navabout').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+
+    $('#navprojects').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 2000);
+    });
+
+    $('#logo').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 1000);
+    });
+
+}
+
+$(document).ready(navbarclick);
