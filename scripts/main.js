a43ade53c07e14f1c5ae43e7ef094fac524487c1
Jnayakk <jnayakk97@gmail.com>
2017-10-09 03:19:48 -0400

    Added projects, resume, contact sections, finished site layout.

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,28 @@ function navbarclick() {
     $('#navprojects').on(('click'), function () {
 
         $('html, body').animate({
-            scrollTop: $("#portfolio").offset().top
+            scrollTop: $("#projects").offset().top
+        }, 2000);
+    });
+
+    $('#navresume').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#resume").offset().top
+        }, 2000);
+    });
+    
+        $('#navskills').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 2000);
+    });
+    
+        $('#navcontact').on(('click'), function () {
+
+        $('html, body').animate({
+            scrollTop: $("#contacts").offset().top
         }, 2000);
     });
 
